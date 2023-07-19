029aa505bdc83bfa1cbb525e663954cc8843c987
parvezlm <parvezstack@gmail.com>
2023-07-19 15:38:21 +0530

    print object value in table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  obj: any = {
+    'name': 'jhon',
+    'age': 28,
+    'company': 'cognizant',
+    'designation': 'angular developer'
+  }
+
+  ngOnInit(): void {
+    this.printData('table')
+  }
+
+  printData(id: string) {
+    for (let item in this.obj) {
+      let tr = document.createElement('tr');
+      tr.innerHTML = `
+        <td>${item}</td>
+        <td>${this.obj[item]}</td>
+      `;
+      document.getElementById(id)?.appendChild(tr);
+    }
+
+  }
+
 }
